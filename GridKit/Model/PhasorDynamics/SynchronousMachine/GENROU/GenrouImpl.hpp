450dca2629a665aa871f7fd3066554d0718fe1f4
lukelowry <wyattluke.lowery@gmail.com>
2026-07-27 20:28:27 -0500

    PERFORMANCE: Remove redundant vars in Genrou

--- GridKit/Model/PhasorDynamics/SynchronousMachine/GENROU/GenrouImpl.hpp
+++ GridKit/Model/PhasorDynamics/SynchronousMachine/GENROU/GenrouImpl.hpp
@@ -49,7 +49,7 @@ namespace GridKit
         S12_(0.),
         mva_base_(100.)
     {
-      size_ = 19;
+      size_ = 17;
       setDerivedParams();
     }
 
@@ -98,7 +98,7 @@ namespace GridKit
         S12_(S12),
         mva_base_(100.)
     {
-      size_ = 19;
+      size_ = 17;
       setDerivedParams();
     }
 
@@ -113,7 +113,7 @@ namespace GridKit
       initializeParameters(data);
       initializeMonitor();
 
-      size_ = 19;
+      size_ = 17;
       setDerivedParams();
     }
 
@@ -130,7 +130,7 @@ namespace GridKit
       initializeParameters(data);
       initializeMonitor();
 
-      size_ = 19;
+      size_ = 17;
       setDerivedParams();
     }
 
@@ -148,7 +148,7 @@ namespace GridKit
       initializeParameters(data);
       initializeMonitor();
 
-      size_ = 19;
+      size_ = 17;
       setDerivedParams();
     }
 
@@ -452,10 +452,6 @@ namespace GridKit
       y[14]       = iq;
       y[15]       = ir;
       y[16]       = ii;
-      y[17]       = G_ * (vd * std::sin(delta) + vq * std::cos(delta))
-              - B_ * (vd * -std::cos(delta) + vq * std::sin(delta));
-      y[18] = B_ * (vd * std::sin(delta) + vq * std::cos(delta))
-              + G_ * (vd * -std::cos(delta) + vq * std::sin(delta));
 
       ScalarT Te = y[12];
       // Convert Te to system base for governor PM signal.
@@ -544,8 +540,6 @@ namespace GridKit
       ScalarT iq     = y[14];
       ScalarT ir     = y[15];
       ScalarT ii     = y[16];
-      ScalarT inr    = y[17];
-      ScalarT ini    = y[18];
 
       /* Read derivatives */
       ScalarT delta_dot = yp[0];
@@ -565,6 +559,14 @@ namespace GridKit
 
       static constexpr auto pi = std::numbers::pi_v<RealT>;
 
+      // Set Rotor Angle commputation
+      const ScalarT sin_delta = std::sin(delta);
+      const ScalarT cos_delta = std::cos(delta);
+
+      // Internal Voltage
+      const ScalarT Vint_r = sin_delta * vd + cos_delta * vq;
+      const ScalarT Vint_i = -cos_delta * vd + sin_delta * vq;
+
       /* 6 Genrou differential equations */
       f[0] = delta_dot - omega * (TWO<RealT> * pi * freq_system_base_);
       f[1] = omega_dot - (ONE<RealT> / (TWO<RealT> * H_)) * ((pmech - D_ * omega) / (ONE<RealT> + omega) - telec);
@@ -573,7 +575,7 @@ namespace GridKit
       f[4] = psiqp_dot - (ONE<RealT> / Tqopp_) * (Edp - psiqp + Xq2_ * iq);
       f[5] = Edp_dot - (ONE<RealT> / Tqop_) * (-Edp + Xqd_ * psiqpp * ksat + Xq1_ * (iq - Xq3_ * (Edp + iq * Xq2_ - psiqp)));
 
-      /* 11 Genrou algebraic equations */
+      /* 9 Genrou algebraic equations */
       f[6]  = psiqpp - (-psiqp * Xq4_ - Edp * Xq5_);
       f[7]  = psidpp - (psidp * Xd4_ + Eqp * Xd5_);
       f[8]  = psipp - std::sqrt((psidpp * psidpp) + (psiqpp * psiqpp));
@@ -581,14 +583,12 @@ namespace GridKit
       f[10] = vd + psiqpp * (ONE<RealT> + omega);
       f[11] = vq - psidpp * (ONE<RealT> + omega);
       f[12] = telec - ((psidpp - id * Xdpp_) * iq - (psiqpp - iq * Xdpp_) * id);
-      f[13] = id - (ir * std::sin(delta) - ii * std::cos(delta));
-      f[14] = iq - (ir * std::cos(delta) + ii * std::sin(delta));
-      f[15] = ir + G_ * vr - B_ * vi - inr;
-      f[16] = ii + B_ * vr + G_ * vi - ini;
+      f[13] = id - (ir * sin_delta - ii * cos_delta);
+      f[14] = iq - (ir * cos_delta + ii * sin_delta);
 
-      /* 2 Genrou current source definitions */
-      f[17] = inr - (G_ * (std::sin(delta) * vd + std::cos(delta) * vq) - B_ * (-std::cos(delta) * vd + std::sin(delta) * vq));
-      f[18] = ini - (B_ * (std::sin(delta) * vd + std::cos(delta) * vq) + G_ * (-std::cos(delta) * vd + std::sin(delta) * vq));
+      /* 2 Genrou network equations */
+      f[15] = ir + G_ * vr - B_ * vi - (G_ * Vint_r - B_ * Vint_i);
+      f[16] = ii + B_ * vr + G_ * vi - (B_ * Vint_r + G_ * Vint_i);
 
       return 0;
     }
