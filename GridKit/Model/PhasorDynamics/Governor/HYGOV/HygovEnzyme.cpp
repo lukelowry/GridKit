a478f55acf155211edb8e52059cd5f5a88818605
lukelowry <wyattluke.lowery@gmail.com>
2026-07-27 20:14:49 -0500

    PERFORMANCE: updateAlpha and updateTime

--- GridKit/Model/PhasorDynamics/Governor/HYGOV/HygovEnzyme.cpp
+++ GridKit/Model/PhasorDynamics/Governor/HYGOV/HygovEnzyme.cpp
@@ -58,7 +58,7 @@ namespace GridKit
                                                                                      yp_.getData(),
                                                                                      wb_.data(),
                                                                                      ws_.data(),
-                                                                                     alpha_,
+                                                                                     alpha(),
                                                                                      J_rows_buffer_,
                                                                                      J_cols_buffer_,
                                                                                      J_vals_buffer_,
--- GridKit/Model/PhasorDynamics/SynchronousMachine/GenClassical/GenClassicalEnzyme.cpp
+++ GridKit/Model/PhasorDynamics/SynchronousMachine/GenClassical/GenClassicalEnzyme.cpp
@@ -61,7 +61,7 @@ namespace GridKit
                                                                                                        y_.getData(),
                                                                                                        yp_.getData(),
                                                                                                        wb_.data(),
-                                                                                                       alpha_,
+                                                                                                       alpha(),
                                                                                                        J_rows_buffer_,
                                                                                                        J_cols_buffer_,
                                                                                                        J_vals_buffer_,
